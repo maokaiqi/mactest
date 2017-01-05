14f9b6408c20bd50830a9eb0753d44f5f119f3a3
毛凯祺 <ethan@EthandeMacBook-Pro.local>
2017-01-06 00:37:51 +0800

    test

---
+++ Untitled.py
@@ -0,0 +1,6 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 22 2016, 17:23:13) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+dd
